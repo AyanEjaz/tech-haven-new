f67feca3b7e1ce10eaa3aeee130d1a7725550bfd
Ejaz Ahmad <ayan.ejaz.420@gmail.com>
2024-04-04 06:17:44 +0500

    tech haven

---
+++ src/payload/seed/image-1.ts
@@ -0,0 +1,40 @@
+export const image1 = {
+  alt: 'Shirts',
+  caption: [
+    {
+      children: [
+        {
+          text: 'Photo by ',
+        },
+        {
+          type: 'link',
+          linkType: 'custom',
+          url: 'https://unsplash.com/@henmankk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText',
+          newTab: true,
+          children: [
+            {
+              text: 'Keagan Henman',
+            },
+          ],
+        },
+        {
+          text: ' on ',
+        },
+        {
+          type: 'link',
+          linkType: 'custom',
+          url: 'https://www.istockphoto.com/photo/3d-rendering-of-shopping-cart-and-smartphone-gm1265991940-A?utm_campaign=srp_photos_bottom&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fmobile-sale&utm_medium=affiliate&utm_source=unsplash&utm_term=mobile+sale%3A%3A%3A',
+          newTab: true,
+          children: [
+            {
+              text: 'Unsplash',
+            },
+          ],
+        },
+        {
+          text: '.',
+        },
+      ],
+    },
+  ],
+}
